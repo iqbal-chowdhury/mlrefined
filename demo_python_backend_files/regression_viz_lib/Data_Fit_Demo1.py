bb1600fa94ad097879752d84638f00ed13f793a3
root <jermwatt@gmail.com>
2017-03-20 18:12:28 +0000

    updates

--- demo_python_backend_files/regression_viz_lib/Data_Fit_Demo1.py
+++ demo_python_backend_files/regression_viz_lib/Data_Fit_Demo1.py
@@ -4,11 +4,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
+
 # import sklearn libraries 
-from sklearn import ensemble
-#from sklearn.preprocessing import PolynomialFeatures
-from sklearn import linear_model
+from sklearn.kernel_ridge import KernelRidge
 from sklearn.neural_network import MLPRegressor
+from sklearn import ensemble
 from sklearn.ensemble import GradientBoostingRegressor
 
 # import widgets and animators
@@ -18,7 +18,8 @@ import sys
 sys.path.append('../')
 
 # import JS animator
-from JSAnimation import IPython_display
+import matplotlib.animation as animation
+from JSAnimation_slider_only import IPython_display
 
 
 class Fit_Bases:
@@ -51,46 +52,41 @@ class Fit_Bases:
         if len(self.x) > 1:
             ax.scatter(self.x,self.y,facecolor = 'k',edgecolor = 'w',linewidth = 1,s = 70)
 
-        plt.xlim(min(self.x)-0.1,max(self.x)+0.1)
-        plt.ylim(min(self.y)-0.1,max(self.y)+0.1)
-        plt.yticks([],[])
-        plt.axis('off')      
-        
-    # plot approximation
-    def plot_approx(self,clf,domain,ax):
-        # use regressor to make predictions
-        z = clf.predict(domain)
-
-        # plot regressor
-        ax.plot(domain,z,linewidth = 3,color = 'b')
-        ax.set_ylim(min(min(self.y)-0.1,min(z)-0.1),max(max(self.y)+0.1,max(z)+0.1))
+        ax.set_xlim(min(self.x)-0.1,max(self.x)+0.1)
+        ax.set_ylim(min(self.y)-0.1,max(self.y)+0.1)
+        ax.set_yticks([],[])
+        ax.axis('off')      
 
     ### demo with animation or sliders - showing function approximation with polynoimal, neural network, and stumps/trees
     # polys
     def browse_poly_fit(self):
         
         # initialize figure
-        fig = plt.figure(figsize = (6,6))
+        fig = plt.figure(figsize = (5,4))
+        artist = fig
         ax = fig.add_subplot(111)
-        
+        r = np.linspace(min(self.x),max(self.x),300)[:, np.newaxis]
+
         def show_fit(num_elements):
+            ax.cla()
+
             # plot our points and target function
             self.plot_all(ax)
             
-            # Create linear regression object
-            poly = PolynomialFeatures(degree=num_elements)
-            x = poly.fit_transform(self.x)
-    
-            clf = linear_model.LinearRegression()
-            clf.fit(x, self.y)        
-
-            ### for polynomial fit --- transform input for proper regression fit
-            r = np.linspace(min(self.x),max(self.x),300)[:, np.newaxis]
-            pr = poly.fit_transform(r)
+            # define classifier object
+            clf = KernelRidge(kernel = 'poly',degree = num_elements)
+            clf.fit(self.x, self.y)        
             
+            # clf = GradientBoostingRegressor(n_estimators=num_elements, learning_rate=1,max_depth=2, random_state=0, loss='ls')
+            
+            # clf = MLPRegressor(solver = 'lbgfs',alpha = 0,activation = 'tanh',random_state = 1,hidden_layer_sizes = (num_elements,num_elements))
+                
             # plot approximation
-            artist = self.plot_approx(clf,r)
+            z = clf.predict(r)
 
+            # plot regressor
+            ax.plot(r,z,linewidth = 3,color = 'b')
+            ax.set_ylim(min(min(self.y)-0.1,min(z)-0.1),max(max(self.y)+0.1,max(z)+0.1))
             return artist,
            
         anim = animation.FuncAnimation(fig, show_fit,frames=20, interval=20, blit=True)
@@ -99,49 +95,4 @@ class Fit_Bases:
         IPython_display.anim_to_html(anim,fps = 20)
         
         return(anim)
-            
-
-    # demo with animation or sliders - showing function approximation with polynoimal, neural network, and stumps/trees
-    def browse_tree_fit(self):
-        def show_fit(num_elements):
-            # plot our points
-            self.plot_all()
-
-            # load in gradient booster
-            clf = GradientBoostingRegressor(n_estimators=num_elements, learning_rate=1,max_depth=2, random_state=0, loss='ls')
-
-            # fit classifier
-            self.y.shape = (len(self.y),)
-            clf.fit(self.x,self.y)
-
-            # plot classification boundary and color regions appropriately
-            r = np.linspace(min(self.x),max(self.x),300)[:, np.newaxis]
-
-            # plot approximation
-            self.plot_approx(clf,r)
-
-        interact(show_fit, num_elements=widgets.IntSlider(min=1,max=20,step=1,value=1))
-        
-      
-    # demo with animation or sliders - showing function approximation with polynoimal, neural network, and stumps/trees
-    def browse_net_fit(self):
-        def show_fit(num_elements):
-            # plot our points
-            self.plot_all()
-
-            # create the decision tree classifier with appropriate 
-            clf = MLPRegressor(solver = 'lbgfs',alpha = 0,activation = 'tanh',random_state = 1,hidden_layer_sizes = (num_elements,num_elements))
-
-            # fit classifier
-            self.y.shape = (len(self.y),)
-            clf.fit(self.x,self.y)
-
-            # plot classification boundary and color regions appropriately
-            r = np.linspace(min(self.x),max(self.x),300)[:, np.newaxis]
-
-            # plot approximation
-            self.plot_approx(clf,r)
-            
-        interact(show_fit, num_elements=widgets.IntSlider(min=1,max=100,step=1,value=1))
-
-
+       
