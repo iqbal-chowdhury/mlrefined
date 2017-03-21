909537668f827dc09e08d28de526e0ce49406a98
root <jermwatt@gmail.com>
2017-03-21 13:58:11 +0000

    consolidation updates

--- demo_python_backend_files/regression_viz_lib/Data_Fit_Demo1.py
+++ demo_python_backend_files/regression_viz_lib/Data_Fit_Demo1.py
@@ -2,11 +2,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import math
-
+import copy
 
 # import sklearn libraries 
-from sklearn.kernel_ridge import KernelRidge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
 from sklearn.neural_network import MLPRegressor
 from sklearn import ensemble
 from sklearn.ensemble import GradientBoostingRegressor
@@ -27,77 +29,182 @@ class Fit_Bases:
     def __init__(self):
         self.x = 0
         self.y = 0
-        self.target_x = []
-        self.target_y = []
-
-    # load in data
-    def load_data(self,csvname):
-        data = np.asarray(pd.read_csv(csvname,header = None))
-        self.x = data[:,0][:, np.newaxis]
-        self.y = data[:,1]
-        
+        self.target_x = 0
+        self.target_y = 0
+    
+    ### data loading functions ###
     # load target function
-    def load_target(self,csvname):
-        data = np.asarray(pd.read_csv(csvname,header = None))
-        self.target_x = data[:,0][:, np.newaxis]
-        self.target_y = data[:,1]
+    def load_target(self,targetcsv):
+        data = np.asarray(pd.read_csv(targetcsv))
+        self.target_x = data[:,0:-1]
+        self.target_y = data[:,-1]
         
+    # load dataset
+    def load_data(self,datacsv):
+        # load data
+        data = np.asarray(pd.read_csv(datacsv))  
+        self.x = data[:,0:-1]
+        self.y = data[:,-1]
+    
+    ### plotting functions ###
     # plot data with underlying target function
-    def plot_all(self,ax):
-        # plot target if loaded
+    def show_setup(self,ax):
+        # plot target if loaded 
         if len(self.target_x) > 1:
-            ax.plot(self.target_x,self.target_y,'r--',linewidth = 2.5,zorder = 0)
-        
-        # plot data if loaded
-        if len(self.x) > 1:
+            # check dimension of plot
+            if np.shape(self.target_x)[1] == 1:  # two-dimensional target function
+                ax.plot(self.target_x,self.target_y,'r--',linewidth = 2.5,zorder = 0)
+            else:                              # three-dimensional target function
+                a = int(math.sqrt(len(self.target_x[:,0])))
+                ax.plot_surface(np.reshape(self.target_x[:,0],(a,a)),np.reshape(self.target_x[:,1],(a,a)),np.reshape(self.target_y,(a,a)),alpha = 0.05,color = 'r',zorder = 0,shade = True,linewidth = 0)
+
+        ### plot data ###
+        # check dimension of plot
+        if np.shape(self.x)[1] == 1:      # two-dimensional target function
             ax.scatter(self.x,self.y,facecolor = 'k',edgecolor = 'w',linewidth = 1,s = 70)
+        else:                             # three-dimensional target function
+            ax.scatter(self.x[:,0],self.x[:,1],self.y,s = 50,color = 'k',edgecolor = 'w')
 
-        ax.set_xlim(min(self.x)-0.1,max(self.x)+0.1)
-        ax.set_ylim(min(self.y)-0.1,max(self.y)+0.1)
+        ### dress panel ###
+        # check dimension of plot 
+        if np.shape(self.x)[1] == 1:      # two-dimensional target function
+            self.panel_2d_cleanup(ax)
+        else:                             # three-dimensional target function
+            self.panel_3d_cleanup(ax)
+
+
+    # function for cleaning up 2-dim panel in a consistent way
+    def panel_2d_cleanup(self,ax):
         ax.set_yticks([],[])
-        ax.axis('off')      
+        ax.axis('off')     
+        
+        # set viewing lims
+        xgap = (max(self.x) - min(self.x))*0.1
+        ygap = (max(self.y) - min(self.y))*0.15
+        ax.set_xlim(min(self.x) - xgap, max(self.x) + xgap)
+        ax.set_ylim(min(self.y) - ygap, max(self.y) + ygap)
+        
+    # function for cleaning up 3-dim panel in a consistent way
+    def panel_3d_cleanup(self,ax):
+        # turn off tick labels
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_zticklabels([])
 
-    ### demo with animation or sliders - showing function approximation with polynoimal, neural network, and stumps/trees
-    # polys
+        # Get rid of the spines on the 3d plot
+        ax.w_xaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
+        ax.w_yaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
+        ax.w_zaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
+
+        # turn off tick marks
+        ax.xaxis.set_tick_params(size=0,color = 'w')
+        ax.yaxis.set_tick_params(size=0,color = 'w')
+        ax.zaxis.set_tick_params(size=0,color = 'w')
+
+        # set lims
+        x1gap = (max(self.x[:,0]) - min(self.x[:,0]))*0.1
+        x2gap = (max(self.x[:,1]) - min(self.x[:,1]))*0.1
+        ygap = (max(self.y) - min(self.y))*0.15
+        
+        ax.set_xlim([min(self.x[:,0])-x1gap,max(self.x[:,0])+x1gap])
+        ax.set_ylim([min(self.x[:,1])-x2gap,max(self.x[:,1])+x2gap])
+        ax.set_zlim([min(self.y)-ygap,max(self.y)+ygap])
+            
+        # set viewing angle
+        ax.view_init(10,-70) 
+        
+    ### animation function ###
     def browse_fit(self,**args):
         # pull out model arg
         model_choice = args['model']
+        if model_choice not in ['poly','nnet','tree']:
+            print 'please try again - choose from: poly, nnet, or tree'
+            return
         
         # pull out range
         param_range = args['param_range']
         
         # initialize figure
-        fig = plt.figure(figsize = (5,4))
+        fig = plt.figure(figsize = (7,7))
         artist = fig
-        ax = fig.add_subplot(111)
-        r = np.linspace(min(self.x),max(self.x),300)[:, np.newaxis]
-
+        
+        # switch - two-d or three-d plot
+        ax = 0
+        r = 0
+        if np.shape(self.x)[1] == 1:     # two-dimensional plot
+            # seed panel
+            ax = fig.add_subplot(111)
+        else:                            # three-dimensional plot
+            ax = plt.subplot(111,projection = '3d')
+            
+        # seed range over which to plot fit - just use input of true function
+        r = copy.deepcopy(self.target_x)
+        
+        # remove whitespace around figure
+        fig.subplots_adjust(left=0,right=1,bottom=0,top=1)          
+        
+        # animation sub-function
         def show_fit(value):
             ax.cla()
 
             # plot our points and target function
-            self.plot_all(ax)
+            self.show_setup(ax)
             
-            # define classifier object
-            clf = 0
+            # define regressor object
+            regressor = 0
+            z = 0
             
-            # choose between models
+            ### choose between models
+            # polynomial
             if model_choice == 'poly':
-                clf = KernelRidge(kernel = 'poly',degree = param_range[value],alpha = 10**-4)
+                # get an instance of the linear regressor + transform input via poly
+                poly = PolynomialFeatures(degree=value+1)
+                x = poly.fit_transform(self.x)
+                regressor = linear_model.LinearRegression()
+                
+                # fit your model to data
+                regressor.fit(x, self.y)   
+                
+                # make prediction
+                pr = poly.fit_transform(r)
+                
+                # produce approximation
+                z = regressor.predict(pr)
+                
+            # neural network
             if model_choice == 'nnet':
-                clf = MLPRegressor(solver = 'lbgfs',alpha = 0,activation = 'tanh',random_state = 1,hidden_layer_sizes = (param_range[value],param_range[value],param_range[value],param_range[value]),tol=10**-5)
+                regressor = MLPRegressor(solver = 'lbgfs',alpha = 0,activation = 'tanh',max_iter = 500,hidden_layer_sizes = (param_range[value],param_range[value],param_range[value],param_range[value]),tol=10**-5)
+                
+                # fit your model to data
+                regressor.fit(self.x, self.y)   
+            
+                # produce approximation
+                z = regressor.predict(r)
+                
+            # tree-based
             if model_choice == 'tree':
-                clf = GradientBoostingRegressor(n_estimators= param_range[value], learning_rate=1,max_depth=2, random_state=0, loss='ls')
+                regressor = GradientBoostingRegressor(n_estimators= param_range[value], learning_rate=1,max_depth=2, random_state=0, loss='ls')
              
-            # fit your model to data
-            clf.fit(self.x, self.y)        
+                # fit your model to data
+                regressor.fit(self.x, self.y)   
 
-            # plot approximation
-            z = clf.predict(r)
+                # produce approximation
+                z = regressor.predict(r)
 
-            # plot regressor
-            ax.plot(r,z,linewidth = 3,color = 'b')
-            ax.set_ylim(min(min(self.y)-0.1,min(z)-0.1),max(max(self.y)+0.1,max(z)+0.1))
+            # plot regressor - two-d or three-d - and clean up plot
+            if np.shape(self.x)[1] == 1:     # two-dimensional plot
+                ax.plot(r,z,linewidth = 3,color = 'b')
+            else:                            # three-dimensional plot
+                a = int(math.sqrt(len(self.target_x[:,0])))
+                ax.plot_surface(np.reshape(self.target_x[:,0],(a,a)),np.reshape(self.target_x[:,1],(a,a)),np.reshape(z,(a,a)),alpha = 0.1,color = 'b',zorder = 0,shade = True,linewidth=0.5,antialiased = True,cstride = 50, rstride = 50)
+            
+            # dress up panel title correctly
+            if value == 0:
+                ax.set_title(model_choice + ' fit with ' + str(value+1) + ' basis element',y=0.9,fontsize = 15)
+            else:
+                ax.set_title(model_choice + ' fit with ' + str(value+1) + ' basis elements',y=0.9,fontsize = 15)
+               
+
             return artist,
            
         anim = animation.FuncAnimation(fig, show_fit,frames=len(param_range), interval=len(param_range), blit=True)
--- demo_python_backend_files/regression_viz_lib/Data_Fit_Demo_3d.py
+++ demo_python_backend_files/regression_viz_lib/Data_Fit_Demo_3d.py
@@ -1,75 +1,50 @@
+# import basic data loading and handling library
 import numpy as np
-import matplotlib.pyplot as plt 
-from mpl_toolkits.mplot3d import Axes3D
 import pandas as pd
-import matplotlib.animation as animation
-from JSAnimation_slider_only import IPython_display
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.neural_network import MLPRegressor
-from sklearn.kernel_ridge import KernelRidge
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import math
 
+# import sklearn libraries 
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
+from sklearn.neural_network import MLPRegressor
+from sklearn import ensemble
+from sklearn.ensemble import GradientBoostingRegressor
+
+# import widgets and animators
+
+# load in other libs
+import sys
+sys.path.append('../')
+
+# import JS animator
+import matplotlib.animation as animation
+from JSAnimation_slider_only import IPython_display
 class two_dim_regression_fits:
     def __init__(self):
-        self.X = 0
+        self.x = 0
         self.y = 0
-        self.x_surf = 0
-        self.y_surf = 0
-        self.z_surf = 0
-        self.regressor = 0
+        self.target_x = 0
+        self.target_y = 0
     
+    # load target function
     def load_target(self,targetcsv):
-        # load underlying function
-        true_func = np.asarray(pd.read_csv(targetcsv))
-        s = true_func[:,0]
-        dim = int(math.sqrt(len(s)))
-        s.shape = (dim,dim)
-        t = true_func[:,1]
-        t.shape = (dim,dim)
-        z_surf = true_func[:,2]
-        z_surf.shape = (dim,dim)
+        data = np.asarray(pd.read_csv(csvname,header = None))
+        self.target_x = data[:,-1]
+        self.target_y = data[:,-1]
         
-        # return globals
-        self.x_surf = s
-        self.y_surf = t
-        self.z_surf = z_surf
-        
+    # load dataset
     def load_data(self,datacsv):
-        # make points
-#         num_pts = 75
-#         X = np.random.rand(num_pts,2)
-#         y = np.sin(2*np.pi*X[:,0]) + 0.1*np.random.randn(num_pts)
-#         y.shape = (len(y),1)
-        
-#         # create underlying surface
-#         r = np.linspace(0,1,500)    
-#         s,t = np.meshgrid(r,r)
-#         s.shape = (np.prod(np.shape(s)),1)
-#         t.shape = (np.prod(np.shape(t)),1)
-#         z_surf = np.sin(2*np.pi*s)
-        
-#         # save everything
-#         data = np.concatenate((X,y), axis = 1)
-#         data = pd.DataFrame(data)
-#         data.to_csv('3d_noisy_sin_sample.csv',index = None)
-        
-#         true_func = np.concatenate((s,t,z_surf),axis = 1)
-#         true_func = pd.DataFrame(true_func)
-#         true_func.to_csv('3d_sin_function.csv',index = None)
-
         # load data
         data = np.asarray(pd.read_csv(datacsv))  
-        X = data[:,0:-1]
-        y = data[:,-1]
-        
-        # return globals
-        self.X = X
-        self.y = y
+        self.x = data[:,0:-1]
+        self.y = data[:,-1]
         
     # plot the data and target
     def plot_basics(self):
         # convert globals
-        X = self.X 
+        x = self.x
         y = self.y 
         s = self.x_surf 
         t =  self.y_surf 
@@ -122,20 +97,30 @@ class two_dim_regression_fits:
         ax1.view_init(10,-70)        
         
     # browse poly fit values
-    def browse_poly_fit(self):
+    def browse_fit(self,**args):
+        # pull out model arg
+        model_choice = args['model']
+        if model_choice not in ['poly','nnet','tree']:
+            print 'please try again - choose from: poly, nnet, or tree'
+            return
+        
+        # pull out range
+        param_range = args['param_range']
+        
         # convert globals
-        X = self.X 
+        x = self.x
         y = self.y 
         s = self.x_surf 
         t =  self.y_surf 
         z_surf = self.z_surf
         
-        # create input for classifier surface fit
+        # create input for regression surface fit
+        
         s_copy = np.asarray(s.copy())
         s_copy.shape = (np.prod(np.shape(s_copy)),1)
         t_copy = np.asarray(t.copy())
         t_copy.shape = (np.prod(np.size(t_copy)),1)
-        h = np.concatenate((s_copy,t_copy),axis = 1)
+        r = np.concatenate((s_copy,t_copy),axis = 1)
         
         #### plot everything ###
         # produce figure
@@ -143,27 +128,62 @@ class two_dim_regression_fits:
         ax1 = plt.subplot(111,projection = '3d')
         fig.subplots_adjust(left=0,right=1,bottom=0,top=1)   # remove whitespace around 3d figure
         
-        def show_fit(step):
+        def show_fit(value):
             ax1.cla()
             ax1.axis('tight')
             
             ### plot all input data ###
             # plot points
-            ax1.scatter(X[:,0],X[:,1],y,s = 50,color = 'k',edgecolor = 'w')
+            ax1.scatter(self.x[:,0],self.x[:,1],self.y,s = 50,color = 'k',edgecolor = 'w')
 
             # plot target surface
             artist = ax1.plot_surface(s,t,z_surf,alpha = 0.05,color = 'r',zorder = 0,shade = True,linewidth = 0)
 
-            ### plot fit ###
-            self.regressor  = KernelRidge(kernel = 'poly',degree = step+1,alpha = 0)
+            ### create regressor and fit to data ###
+            ### choose between models
+            # polynomial
+            if model_choice == 'poly':
+                poly = PolynomialFeatures(degree=value+1)
+                x = poly.fit_transform(self.x)
+                regressor = linear_model.LinearRegression()
+                
+                # fit your model to data
+                regressor.fit(x, self.y)   
+                
+                # make prediction
+                pr = poly.fit_transform(r)
+                
+                # produce approximation
+                z = regressor.predict(pr)
+                
+            # neural network
+            if model_choice == 'nnet':
+                regressor = MLPRegressor(solver = 'lbgfs',alpha = 0,activation = 'tanh',max_iter = 500,hidden_layer_sizes = (param_range[value],param_range[value],param_range[value],param_range[value]),tol=10**-5)
+                
+                # fit your model to data
+                regressor.fit(self.x, self.y)   
             
-            self.regressor.fit(X,y)
-            z_output = self.regressor.predict(h)
-            z_output.shape = (np.shape(s))
-            ax1.plot_surface(s,t,z_output,alpha = 0.1,color = 'b',zorder = 0,shade = True,linewidth=0.5,antialiased = True,cstride = 50, rstride = 50)
+                # produce approximation
+                z = regressor.predict(r)
+                
+            # tree-based
+            if model_choice == 'tree':
+                regressor = GradientBoostingRegressor(n_estimators= param_range[value], learning_rate=1,max_depth=1, random_state=0, loss='ls')
+             
+                # fit your model to data
+                regressor.fit(self.x, self.y)   
+
+                # produce approximation
+                z = regressor.predict(r)
+            
+            ### plot surface of prediction
+            z.shape = (np.shape(s))
+            
+            ### plot regression surface ###
+            ax1.plot_surface(s,t,z,alpha = 0.1,color = 'b',zorder = 0,shade = True,linewidth=0.5,antialiased = True,cstride = 50, rstride = 50)
 
             ### clean up plot ###
-            ax1.set_title('tree fit with ' + str(step+1) + ' basis elements')
+            ax1.set_title('tree fit with ' + str(value+1) + ' basis elements')
             
             # turn off tick labels
             ax1.set_xticklabels([])
@@ -189,161 +209,11 @@ class two_dim_regression_fits:
             ax1.view_init(10,-70)        
 
             return artist,
-           
-        anim = animation.FuncAnimation(fig, show_fit,frames=10, interval=10, blit=True)
+        
+        anim = animation.FuncAnimation(fig, show_fit,frames=len(param_range), interval=len(param_range), blit=True)
         
         # set frames per second in animation
-        IPython_display.anim_to_html(anim,fps = 10)
+        IPython_display.anim_to_html(anim,fps = len(param_range))
         
         return(anim)
-        
-    # browse neural net values
-    def browse_net_fit(self):
-        # convert globals
-        X = self.X 
-        y = self.y 
-        s = self.x_surf 
-        t =  self.y_surf 
-        z_surf = self.z_surf
-        
-        # create input for classifier surface fit
-        s_copy = np.asarray(s.copy())
-        s_copy.shape = (np.prod(np.shape(s_copy)),1)
-        t_copy = np.asarray(t.copy())
-        t_copy.shape = (np.prod(np.size(t_copy)),1)
-        h = np.concatenate((s_copy,t_copy),axis = 1)
-        
-        #### plot everything ###
-        # produce figure
-        fig = plt.figure(num=None, figsize=(5,5), dpi=80, facecolor='w', edgecolor='k')
-        ax1 = plt.subplot(111,projection = '3d')
-        fig.subplots_adjust(left=0,right=1,bottom=0,top=1)   # remove whitespace around 3d figure
-        
-        def show_fit(step):
-            ax1.cla()
-            ax1.axis('tight')
-            
-            ### plot all input data ###
-            # plot points
-            ax1.scatter(X[:,0],X[:,1],y,s = 50,color = 'k',edgecolor = 'w')
-
-            # plot target surface
-            artist = ax1.plot_surface(s,t,z_surf,alpha = 0.05,color = 'r',zorder = 0,shade = True,linewidth = 0)
-
-            ### plot fit ###
-            self.regressor = MLPRegressor(solver = 'lbfgs',alpha = 0,activation = 'tanh',random_state = 1,hidden_layer_sizes = (step+1,step+1))
-            
-            self.regressor.fit(X,y)
-            z_output = self.regressor.predict(h)
-            z_output.shape = (np.shape(s))
-            ax1.plot_surface(s,t,z_output,alpha = 0.1,color = 'b',zorder = 0,shade = True,linewidth=0.5,antialiased = True,cstride = 50, rstride = 50)
-
-            ### clean up plot ###
-            ax1.set_title('tree fit with ' + str(step+1) + ' basis elements')
-            
-            # turn off tick labels
-            ax1.set_xticklabels([])
-            ax1.set_yticklabels([])
-            ax1.set_zticklabels([])
-
-            # Get rid of the spines on the 3d plot
-            ax1.w_xaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
-            ax1.w_yaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
-            ax1.w_zaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
-
-            # turn off tick marks
-            ax1.xaxis.set_tick_params(size=0,color = 'w')
-            ax1.yaxis.set_tick_params(size=0,color = 'w')
-            ax1.zaxis.set_tick_params(size=0,color = 'w')
-            
-            # set limits
-            ax1.set_xlim([-0.05,1.05])
-            ax1.set_ylim([-0.05,1.05])
-            ax1.set_zlim([-1.1,1.1])
-
-            # set viewing angle
-            ax1.view_init(10,-70)        
-
-            return artist,
-           
-        anim = animation.FuncAnimation(fig, show_fit,frames=20, interval=20, blit=True)
-        
-        # set frames per second in animation
-        IPython_display.anim_to_html(anim,fps = 20)
-        
-        return(anim)
-    
-    # browse tree values
-    def browse_tree_fit(self):
-        # convert globals
-        X = self.X 
-        y = self.y 
-        s = self.x_surf 
-        t =  self.y_surf 
-        z_surf = self.z_surf
-        
-        # create input for classifier surface fit
-        s_copy = np.asarray(s.copy())
-        s_copy.shape = (np.prod(np.shape(s_copy)),1)
-        t_copy = np.asarray(t.copy())
-        t_copy.shape = (np.prod(np.size(t_copy)),1)
-        h = np.concatenate((s_copy,t_copy),axis = 1)
-        
-        #### plot everything ###
-        # produce figure
-        fig = plt.figure(num=None, figsize=(5,5), dpi=80, facecolor='w', edgecolor='k')
-        ax1 = plt.subplot(111,projection = '3d')
-        fig.subplots_adjust(left=0,right=1,bottom=0,top=1)   # remove whitespace around 3d figure
-        
-        def show_fit(step):
-            ax1.cla()
-            ax1.axis('tight')
-            
-            ### plot all input data ###
-            # plot points
-            ax1.scatter(X[:,0],X[:,1],y,s = 50,color = 'k',edgecolor = 'w')
-
-            # plot target surface
-            artist = ax1.plot_surface(s,t,z_surf,alpha = 0.05,color = 'r',zorder = 0,shade = True,linewidth = 0)
-
-            ### plot fit ###
-            self.regressor = GradientBoostingRegressor(n_estimators=step+1, learning_rate=1,max_depth=1, random_state=0, loss='ls')
-            self.regressor.fit(X,y)
-            z_output = self.regressor.predict(h)
-            z_output.shape = (np.shape(s))
-            ax1.plot_surface(s,t,z_output,alpha = 0.1,color = 'b',zorder = 0,shade = True,linewidth=0.5,antialiased = True,cstride = 50, rstride = 50)
-
-            ### clean up plot ###
-            ax1.set_title('tree fit with ' + str(step+1) + ' basis elements')
-            
-            # turn off tick labels
-            ax1.set_xticklabels([])
-            ax1.set_yticklabels([])
-            ax1.set_zticklabels([])
-
-            # Get rid of the spines on the 3d plot
-            ax1.w_xaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
-            ax1.w_yaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
-            ax1.w_zaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
-
-            # turn off tick marks
-            ax1.xaxis.set_tick_params(size=0,color = 'w')
-            ax1.yaxis.set_tick_params(size=0,color = 'w')
-            ax1.zaxis.set_tick_params(size=0,color = 'w')
-            
-            # set limits
-            ax1.set_xlim([-0.05,1.05])
-            ax1.set_ylim([-0.05,1.05])
-            ax1.set_zlim([-1.1,1.1])
-
-            # set viewing angle
-            ax1.view_init(10,-70)        
-
-            return artist,
-           
-        anim = animation.FuncAnimation(fig, show_fit,frames=20, interval=20, blit=True)
-        
-        # set frames per second in animation
-        IPython_display.anim_to_html(anim,fps = 20)
-        
-        return(anim)
+ 
